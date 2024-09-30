447d988f3a20bf83cf1d8c90d723fbb369712803
TaeSan Choi <xotks7524@gmail.com>
2024-09-30 16:23:11 +0900

    Refactor: SQL 쿼리 최적화

--- src/main/java/edu/sm/frame/Sql.java
+++ src/main/java/edu/sm/frame/Sql.java
@@ -20,8 +20,6 @@ public class Sql {
             "DELETE FROM customer WHERE cust_id = ?";
 
     // 통계 및 분석 (SA)
-
-
     public static String selectViewSalesStatistics =
             "SELECT * FROM sales_statistics";
 
@@ -85,44 +83,6 @@ public class Sql {
                     "GROUP BY YEAR(order_date)";
 
 
-
-    // 시스템 설정 (SS)
-    public static String manageGeneralSettings =
-            "SELECT * FROM system_settings";
-
-    public static String managePaymentSettings =
-            "SELECT * FROM payment_settings";
-
-    public static String manageShippingSettings =
-            "SELECT * FROM shipping_settings";
-
-    public static String setPaymentFees =
-            "UPDATE payment_method SET fee = ? WHERE method_id = ?";
-
-    public static String viewPaymentMethods =
-            "SELECT * FROM payment_method";
-
-    public static String addPaymentMethod =
-            "INSERT INTO payment_method (name, fee) VALUES (?, ?)";
-
-    public static String editPaymentMethod =
-            "UPDATE payment_method SET name = ?, fee = ? WHERE method_id = ?";
-
-    public static String deletePaymentMethod =
-            "DELETE FROM payment_method WHERE method_id = ?";
-
-    public static String viewShippingCompanies =
-            "SELECT * FROM shipping_company";
-
-    public static String addShippingCompany =
-            "INSERT INTO shipping_company (name, tracking_url) VALUES (?, ?)";
-
-    public static String editShippingCompany =
-            "UPDATE shipping_company SET name = ?, tracking_url = ? WHERE company_id = ?";
-
-    public static String deleteShippingCompany =
-            "DELETE FROM shipping_company WHERE company_id = ?";
-
     // 상품 관리 (PM)
     public static String searchProducts =
             "SELECT * FROM product WHERE product_id = ?";
@@ -133,15 +93,6 @@ public class Sql {
     public static String registerProduct =
             "INSERT INTO product (category_id, name, price, reg_date, description, img1, img2, img3, img4, img5, count, is_public) " +
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-    public static String manageCategories =
-            "SELECT * FROM category";
-
-    public static String manageProductInquiries =
-            "SELECT * FROM product_inquiry WHERE product_id = ?";
-
-    public static String viewProductDetails =
-            "SELECT * FROM product WHERE product_id = ?";
 
     public static String editProduct =
             "UPDATE product SET category_id = ?, name = ?, price = ?, description = ?, " +
@@ -160,11 +111,6 @@ public class Sql {
     public static String deleteCategory =
             "DELETE FROM category WHERE category_id = ?";
 
-    public static String listProductInquiries =
-            "SELECT * FROM product_inquiry WHERE product_id = ?";
-
-    public static String answerInquiry =
-            "UPDATE product_inquiry SET answer = ?, answer_date = NOW() WHERE inquiry_id = ?";
 
     // 주문 관리 (OM)
     public static String insertOrder =
@@ -178,14 +124,10 @@ public class Sql {
     public static String listOrders =
             "SELECT * FROM `order`";
 
-    public static String viewOrderDetails =
-            "SELECT o.*, od.* FROM `order` o JOIN order_detail od ON o.order_id = od.order_id WHERE o.order_id = ?";
 
     public static String listDelivery =
             "SELECT * FROM delivery";
 
-    public static String managePayments =
-            "SELECT * FROM payment WHERE order_id = ?";
 
     public static String editOrder =
             "UPDATE `order` SET product_count = ?, price = ?, name = ?, phone = ?, " +
@@ -204,11 +146,6 @@ public class Sql {
                     "WHERE delivery_id = ?";
     public static String insertDelivery =
             "INSERT INTO delivery (order_id, status, tracking_number, estimated_delivery, actual_delivery) VALUES (?, ?, ?, ?, ?)";
-    public static String viewPaymentHistory =
-            "SELECT * FROM payment WHERE order_id = ?";
-
-    public static String processRefund =
-            "INSERT INTO refund (order_id, amount, reason) VALUES (?, ?, ?)";
 
     // OrderDetail 관련 쿼리
     public static String insertOrderDetail =
@@ -226,41 +163,6 @@ public class Sql {
     public static String selectAllOrderDetails =
             "SELECT * FROM order_detail";
 
-    // 상품 (PD)
-    public static String showPopularProducts =
-            "SELECT p.*, COUNT(od.product_id) as sales_count " +
-                    "FROM product p LEFT JOIN order_detail od ON p.product_id = od.product_id " +
-                    "GROUP BY p.product_id ORDER BY sales_count DESC LIMIT 10";
-
-    public static String setCategorySystem =
-            "SELECT * FROM category";
-
-    public static String displayProductList =
-            "SELECT * FROM product WHERE category_id = ? LIMIT ? OFFSET ?";
-
-
-    public static String displayReviewsAndRatings =
-            "SELECT * FROM review WHERE product_id = ?";
-
-    public static String showRelatedProducts =
-            "SELECT * FROM product WHERE category_id = ? AND product_id != ? LIMIT 5";
-
-    public static String addReview =
-            "INSERT INTO review (product_id, cust_id, rating, content) VALUES (?, ?, ?, ?)";
-
-    public static String editReview =
-            "UPDATE review SET rating = ?, content = ? WHERE review_id = ? AND cust_id = ?";
-
-    public static String deleteReview =
-            "DELETE FROM review WHERE review_id = ? AND cust_id = ?";
-
-    public static String addProductInquiry =
-            "INSERT INTO product_inquiry (product_id, cust_id, content) VALUES (?, ?, ?)";
-
-    public static String deleteProductInquiry =
-            "DELETE FROM product_inquiry WHERE inquiry_id = ? AND cust_id = ?";
-
-    // 장바구니 (CT)
 
     // 장바구니 (Cart) 관련 쿼리
     public static String insertCart =
@@ -278,38 +180,7 @@ public class Sql {
     public static String selectAllCarts =
             "SELECT * FROM cart";
 
-    public static String selectCartsByCustomer =
-            "SELECT * FROM cart WHERE cust_id = ?";
-    public static String displayCartList =
-            "SELECT c.*, p.name, p.price FROM cart c JOIN product p ON c.product_id = p.product_id WHERE c.cust_id = ?";
-
-    public static String processPayment =
-            "INSERT INTO payment (order_id, amount, method) VALUES (?, ?, ?)";
-
-    public static String changeProductQuantity =
-            "UPDATE cart SET count = ? WHERE cart_id = ? AND cust_id = ?";
-
-    public static String deleteCartItem =
-            "DELETE FROM cart WHERE cart_id = ? AND cust_id = ?";
-
-    public static String enterShippingInfo =
-            "UPDATE `order` SET name = ?, phone = ?, address1 = ?, address2 = ?, zip_code = ?, request = ? " +
-                    "WHERE order_id = ?";
-
-    public static String selectPaymentMethod =
-            "UPDATE `order` SET card = ? WHERE order_id = ?";
-
-    public static String confirmOrder =
-            "SELECT * FROM `order` WHERE order_id = ?";
-
-
     // 마이페이지 (MP)
-    public static String viewOrderHistory =
-            "SELECT * FROM `order` WHERE cust_id = ? ORDER BY order_date DESC";
-
-    public static String managePersonalInfo =
-            "SELECT * FROM customer WHERE cust_id = ?";
-
     public static String listAddresses =
             "SELECT * FROM address";
     public static String listAddressesDetails =
@@ -350,19 +221,6 @@ public class Sql {
 
     public static String deleteAddress =
             "DELETE FROM address WHERE address_key = ?";
-
-    public static String viewEarnedMileage =
-            "SELECT * FROM mileage WHERE cust_id = ? ORDER BY date DESC";
-
-    public static String viewUsedMileage =
-            "SELECT * FROM used_mileage WHERE cust_id = ? ORDER BY used_date DESC";
-
-    public static String deleteWishItem =
-            "DELETE FROM wish WHERE wish_id = ? AND cust_id = ?";
-
-    public static String addToCartFromWishList =
-            "INSERT INTO cart (cust_id, product_id, count) " +
-                    "SELECT cust_id, product_id, 1 FROM wish WHERE wish_id = ? AND cust_id = ?";
 
 
     // Board 관련 쿼리
@@ -448,10 +306,5 @@ public class Sql {
 
     public static String selectAllWishes =
             "SELECT * FROM wish";
-
-
-
-
-
 
 }
